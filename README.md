43163b1b9c0f80fddfde07aa91b42e9b2186e29d
nyghtowl <melanie@change.org>
2015-02-15 16:57:09 -0800

    Added graphlab example from tutorial on MNIST.

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 Neural_Net_Newbies
 ==================
 
-Placeholder for simple neural net example.
+This repository holds code that will be used in my PyCon 2015 presentation.
+
+
+Code samples for different Python packages that run Neural Networks.
+
+
+Dato/Graphlab:
+--------
+MNIST sample tutorial can be found at this [link](https://dato.com/products/create/docs/graphlab.toolkits.deeplearning.html)
+
+Image tutorial can be found at this [link](https://dato.com/learn/gallery/notebooks/build_imagenet_deeplearning.html)
+
+Basic architectures:
+    * Perceptron Network for dense numeric input
+    * Convolution Network for image data input
 
 Resources:
-
+--------
 [Deep Learning for NLP] (http://techtalks.tv/talks/deep-learning-for-nlp-without-magic-part-1/58414/)
 [Deep Learning: Machine Perception and Its Applications] (https://www.youtube.com/watch?v=hykoKDl1AtE)
---
+++ code/graphlab_tutorial.py
@@ -0,0 +1,73 @@
+'''
+Graphlab 1 Layer Convolution Net MNIST Example / Tutorial
+
+'''
+
+import graphlab
+import os
+import tarfile
+
+def load_data(filename):
+    '''
+    Graphlab stored a train/test split of the data on S3. 
+
+    You can change the split by combing the data and splitting again.
+
+    4 Minutes to load.
+    '''
+    return graphlab.SFrame(filename)
+    #data['image'] = graphlab.image_analysis.resize(training_data['image'], 28, 28, 1) # potentialy need to resize data.
+
+def create_structure():
+    '''
+    Tune neural net hyper parameters.
+
+    Creates the NN model template which is similar to creating the y=mx+b model template for linear regression.
+
+    Optional and fundamentally using convolutional neural nets either way.
+    '''
+    structure = graphlab.deeplearning.get_builtin_neuralnet('mnist')
+    print structure.layers # Details on layers
+    print structure.params # Hyper parameters summary
+
+    return structure
+
+def train_model(data, net):
+    '''
+    Create convolutional NN 1 layer model.
+    '''
+    return graphlab.neuralnet_classifier.create(data, target='label', network = net, max_iterations=3)
+
+def get_features():
+    '''
+    SArray of dense feature vectors, each of which is the concatenation of all the hidden unit values.
+    '''
+    return model.extract_features()
+
+def predict_values(model, data):
+    return model.classify(data)
+
+def evaluate_model(model, test_data):
+    '''
+    Error rate between predictions and labels
+    '''
+    return model.evaluate(test_data)
+
+
+def main(set_net_structure=True):
+    train_data = load_data('http://s3.amazonaws.com/GraphLab-Datasets/mnist/sframe/train')
+    test_data = load_data('http://s3.amazonaws.com/GraphLab-Datasets/mnist/sframe/test')
+
+    if set_net_structure:
+        structure = create_structure()
+    else:
+        structure = None
+
+    model = train_model(train_data, structure)
+
+    print evaluate_model(model, test_data)
+
+
+
+if __name__ == '__main__':
+    main()
