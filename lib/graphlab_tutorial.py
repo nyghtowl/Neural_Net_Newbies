54586ba07a6e8d77000415b5d277c6d4779f70cf
nyghtowl <melanie@change.org>
2015-03-23 09:34:00 -0700

    Testing graphlab manually with ipython and worked succesfully. Updated stand alone code to reflect changes but hitting an error in trainig the model before it completes.

--- lib/graphlab_tutorial.py
+++ lib/graphlab_tutorial.py
@@ -18,9 +18,6 @@ def load_data(filename):
     return graphlab.SFrame(filename)
     #data['image'] = graphlab.image_analysis.resize(training_data['image'], 28, 28, 1) # potentialy need to resize data.
 
-def resize_data(data):
-    return graphlab.image_analysis.resize(data['image'], 28, 28, 1)
-
 def create_structure():
     '''
     Tune neural net hyper parameters.
@@ -30,8 +27,8 @@ def create_structure():
     Optional and fundamentally using convolutional neural nets either way.
     '''
     structure = graphlab.deeplearning.get_builtin_neuralnet('mnist')
-    print structure.layers # Details on layers
-    print structure.params # Hyper parameters summary
+    print "NN layer details: ", structure.layers 
+    print "NN hyper parameters summary: ", structure.params
 
     return structure
 
@@ -39,17 +36,22 @@ def train_model(data, net):
     '''
     Create convolutional NN 1 layer model.
 
-    Model automatically sets validation to check model performance
+    Model automatically sets validation to check model performance when training
     '''
-    return graphlab.neuralnet_classifier.create(data, target='label', network = net, max_iterations=3)
+    return graphlab.neuralnet_classifier.create(data, target='label', network = net, max_iterations=3, validation_set=None)
 
 def get_features(data):
     '''
-    SArray of dense feature vectors, each of which is the concatenation of all the hidden unit values.
+    SArray of dense feature vectors, each of which is the concatenation of all the hidden unit values
     '''
     return model.extract_features(data)
 
 def predict_values(model, data):
+    '''
+    Predict classification based on input pixels
+
+    Input a picture of a digit and output a prediciton 
+    '''
     return model.classify(data)
 
 def evaluate_model(model, test_data):
@@ -57,9 +59,20 @@ def evaluate_model(model, test_data):
     Error rate between predictions and labels
     '''
     eval_ = model.evaluate(test_data)
+    print "Accuracy: ", eval_['accuracy']
 
+    cf_mat = eval_['confusion_matrix']
+    
+    print "Confusion Matrix True Positive",
+    cf_mat[cf_mat['target_label'] == cf_mat['predicted_label']].groupby('target_label', graphlab.aggregate.SUM('count')).sort('target_label')
+
+    print "Confusion Matrix True Negative",
+    cf_mat[cf_mat['target_label'] != cf_mat['predicted_label']].groupby('target_label', graphlab.aggregate.SUM('count')).sort('target_label')
 
 def main(set_net_structure=True):
+    '''
+    Runs the full program to train the model and then evaluate
+    '''
     train_data = load_data('http://s3.amazonaws.com/GraphLab-Datasets/mnist/sframe/train')
     test_data = load_data('http://s3.amazonaws.com/GraphLab-Datasets/mnist/sframe/test')
 
@@ -70,8 +83,9 @@ def main(set_net_structure=True):
 
     model = train_model(train_data, structure)
 
-    print evaluate_model(model, test_data)
+    evaluate_model(model, test_data)
 
+    print "Evaluation completed"
 
 
 if __name__ == '__main__':
