02c9343c2f8509f7c364c74f9b130e695753631f
nyghtowl <melanie@change.org>
2015-04-08 21:28:52 -0400

    Fixed scikit-learn example. Must combine with another ML model like logistic regression to run predict on new data.

--- lib/sklearn_mnist.py
+++ lib/sklearn_mnist.py
@@ -1,16 +1,14 @@
 '''
 Scikit-Learn RBM MNIST Example / Tutorial
 
-Note: Found issue with running predictions on rbm structure. This script does not run fully.
-
-Utilized code from to help: http://www.pyimagesearch.com/2014/06/23/applying-deep-learning-rbm-mnist-using-python/
 '''
 # import the necessary packages
 from sklearn.datasets import fetch_mldata
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
 from sklearn.neural_network import BernoulliRBM
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
 import numpy as np
 
 def load_data(datasetPath='data/'):
@@ -22,38 +20,11 @@ def scale(X, eps = 0.001):
     return (X - np.min(X, axis = 0)) / (np.max(X, axis = 0) + eps)
 
 def split_data(data, test_size, random_state):
-    X = mnist['data']
-    y = mnist['target']
+    X = data['data']
+    y = data['target']
     X = X.astype("float32")
     X = scale(X)
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-def nudge(X, y):
-    # initialize the translations to shift the image one pixel
-    # up, down, left, and right, then initialize the new data
-    # matrix and targets
-    translations = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-    data = []
-    target = []
-
-    # loop over each of the digits
-    for (image, label) in zip(X, y):
-        # reshape the image from a feature vector of 784 raw
-        # pixel intensities to a 28x28 'image'
-        image = image.reshape(28, 28)
-
-        # loop over the translations
-        for (tX, tY) in translations:
-            # translate the image
-            M = np.float32([[1, 0, tX], [0, 1, tY]])
-            trans = cv2.warpAffine(image, M, (28, 28))
-
-            # update the list of data and target
-            data.append(trans.flatten())
-            target.append(label)
-
-    # return a tuple of the data matrix and targets
-    return (np.array(data), np.array(target))
 
 def main(test_size=0.1, n_iter = 10, learning_rate = 0.01):
 
@@ -64,20 +35,26 @@ def main(test_size=0.1, n_iter = 10, learning_rate = 0.01):
     dataset = load_data()
     trainX, testX, trainY, testY = split_data(dataset, test_size=test_size, random_state=42)
 
-    print '... building the model'
+    print '... building the model structure'
+    # initialize the RBM + Logistic Regression classifier with the cross-validated parameters
+
     rbm = BernoulliRBM(n_components = n_components, n_iter = n_iter,
         learning_rate = learning_rate)
 
+    logistic = LogisticRegression(C = 1.0)
+
     print '... training the model'
-    rbm.fit(trainX, trainY)
+    # train the classifier and show an evaluation report
+    classifier = Pipeline([("rbm", rbm), ("logistic", logistic)])
+    classifier.fit(trainX, trainY)
+
+    # rbm.fit(trainX, trainY)
 
     print '... evaluate model' 
     # Predict does not exist and its not clear how to get value out
-    print classification_report(testY, rbm.predict(testX))
+    print classifier.score(testX, testY)
 
-    # To get variations on test data for evaluation
-    testX, testY = nudge(testX, testY)
-    print classification_report(testY, rbm.predict(testX))
+
 
 if __name__ == '__main__':
     main()
